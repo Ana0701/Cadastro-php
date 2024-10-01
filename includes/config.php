93418e968313cec1e193c50ab52fe95f5c915dd2
Ana0701 <ar9004296@gmail.co>
2024-10-01 07:33:27 -0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$host = 'localhost';
+$usuario = 'root';
+$senha = 'root';
+$banco = 'estoque';
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+
+
+
+if ($conn->connect_error) {
+  die("falha na conexão: " . $conn->connect_error);
+}
+
